2dc82715c9848abc4da726ada9fc0c7463e13abb
bryxosmmm <bryxosmmm@proton.me>
2026-05-25 23:05:36 +0300

    feat: _tests_

---
+++ test/test_narya.ml
@@ -0,0 +1,10 @@
+open! Core
+module A = Narya.Ndarray
+
+let print_array f v = print_s (Array.sexp_of_t f v)
+
+let%expect_test "create zeros" =
+  let x = A.zeros [| 2; 2 |] in
+  print_array Float.sexp_of_t (A.to_array x);
+  [%expect {| (0 0 0 0) |}]
+;;
